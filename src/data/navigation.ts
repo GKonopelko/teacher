8b91bc42b4610ab3e7d62f6897a97276ac704e06
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-10-02 22:14:23 +0300

    fix: remove menu items

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,9 +1,5 @@
 export const navigationLinks = [
-  { href: '/', label: 'Главная' },
   { href: '/about/', label: 'Обо мне' },
-  { href: '/lessons/', label: 'Уроки' },
-  { href: '/grammar/', label: 'Грамматика' },
-  { href: '/dictionary/', label: 'Словарь' },
-  { href: '/level-test/', label: 'Тест уровня' },
+  { href: '/materials/', label: 'Материалы' },
   { href: '/contact/', label: 'Контакты' },
 ];
