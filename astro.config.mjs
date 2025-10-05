cbd53a95d1aae8cf490d7aa7d9675c038ea48bbb
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-10-05 14:58:12 +0300

    refactor: styles to rem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { resolve } from 'path';
 
 export default defineConfig({
   site: 'https://grigori.konopelko.com/',
   base: '/',
   trailingSlash: 'always',
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': resolve('./src/assets'),
+      },
+    },
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
 });
